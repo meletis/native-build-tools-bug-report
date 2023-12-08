3eaa1286a0e930e1f4227929f545a11ddf0aa18a
Meletis Margaritis <meletis@surveycto.com>
2023-12-08 15:37:53 +0200

    Added @DisabledInAotMode as adviced.

--- src/test/java/org/example/SecondTest.java
+++ src/test/java/org/example/SecondTest.java
@@ -3,7 +3,9 @@ package org.example;
 import org.example.service.MyService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.aot.DisabledInAotMode;
 
+@DisabledInAotMode
 public class SecondTest extends BaseTest {
 
     @MockBean
